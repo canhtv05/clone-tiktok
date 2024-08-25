45bb0729c16bec777102545903ab9c1ce5eafdf5
canhtv05 <canhtvph51036@gmail.com>
2024-08-25 22:21:08 +0700

    add styleLint

---
+++ StepToBuildAWebsite.md
@@ -0,0 +1,13 @@
+## 1. Cài đặt customize-cra để tùy chỉnh web pack
+
+## 2. Cài đặt babel-plugin-module-resolver custom path / babel customize github
+
+## 3. Config pretty với file pretterrc
+
+## 4. Config CSS/SASS
+
+## 5. Config Router/LayoutDefault
+
+## 6. Cài đặt thư viện classnames
+
+## 7. Cài đặt tippyJs để có thể sử dụng như trạng thái hover hay focus show ra 1 frame mới
---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,62 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import Tippy from '@tippyjs/react/headless';
+
+import Button from '~/components/Button';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import AccountItem from '~/components/AccountItem';
+
+const cx = classNames.bind(styles);
+
+function Header() {
+    const [searchRes, setSearchRes] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchRes([]);
+        }, 0);
+    });
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="TikTok" />
+                <Tippy
+                    interactive
+                    visible={searchRes.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Account</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search accounts and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('actions')}>
+                    <Button>Login</Button>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
